3d1b0ce4e28a0a6541be435ab165ad8f3f111b1b
happy663 <tatu2425@gmail.com>
2024-10-22 19:16:59 +0900

    Refactor ThreeDimensionalEstimator to estimate 3D trajectory with barometric data

--- src/palkia/positioning/pdr/three_dimensional_estimator.py
+++ src/palkia/positioning/pdr/three_dimensional_estimator.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 
+from palkia.const import TIMESTAMP
+
 from .pdr_estimator import PDREstimator
 
 
@@ -10,21 +12,17 @@ class ThreeDimensionalEstimator:
 
     def estimate_3d_trajectory(
         self,
-        acc_data: pd.DataFrame,
-        gyro_data: pd.DataFrame,
         baro_data: pd.DataFrame,
     ) -> pd.DataFrame:
         # 2D軌跡の推定
-        trajectory_2d = self.pdr_estimator.estimate_trajectory(acc_data, gyro_data)
-
+        trajectory_2d = self.pdr_estimator.estimate_trajectory()
         # 気圧データから高度を推定
         height = self.__estimate_height_from_pressure(baro_data)
-
         # 3D軌跡の生成
         trajectory_3d = trajectory_2d.copy()
         trajectory_3d["z"] = np.interp(
-            trajectory_3d["timestamp"],
-            baro_data["timestamp"],
+            trajectory_3d[TIMESTAMP],
+            baro_data[TIMESTAMP],
             height,
         )
 
